8d3c5af132ad9865e5c51d9b73555d172645d072
Louis-LJ <79785663+Louis-LJ@users.noreply.github.com>
2024-06-05 12:49:45 +0200

    test

--- Script.js
+++ Script.js
@@ -28,6 +28,36 @@ navbarLinksContainer.addEventListener("click", (clickEvent) => {
 
 navbarMenu.addEventListener("click", closeMobileNavbar);
 
+const navbar = document.getElementById("navbar");
+const navbarTogglelang = navbar.querySelector(".navbar-toggle-lang");
+
+function openMobileNavbar() {
+  navbar.classList.add("opened");
+  navbarTogglelang.setAttribute("aria-expanded", "true");
+}
+
+function closeMobileNavbar() {
+  navbar.classList.remove("opened");
+  navbarTogglelang.setAttribute("aria-expanded", "false");
+}
+
+navbarTogglelang.addEventListener("click", () => {
+  if (navbar.classList.contains("opened")) {
+    closeMobileNavbar();
+  } else {
+    openMobileNavbar();
+  }
+});
+
+const navbarMenu = navbar.querySelector("#navbar-menu");
+const navbarLinksContainer = navbar.querySelector(".navbar-links");
+
+navbarLinksContainer.addEventListener("click", (clickEvent) => {
+  clickEvent.stopPropagation();
+});
+
+navbarMenu.addEventListener("click", closeMobileNavbar);
+
 document
   .getElementById("options")
   .querySelectorAll("input[name='navtype']")
